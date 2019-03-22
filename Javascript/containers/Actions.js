85a2d45b8743655a424f0ef4debce3e66f8a7f54
nadermokhtar <nader.mokhtar@gmail.com>
2019-03-21 22:56:38 -0400

    style improved

--- Javascript/containers/Actions.js
+++ Javascript/containers/Actions.js
@@ -8,9 +8,31 @@ export default class Actions extends React.Component {
   }
   render() {
     let actions = this.props.actions.map(action => {
-      return <Text style={{
-        backgroundColor: 'red'}}>{action}</Text>;
+      return <Text style={styles.item}>{action}</Text>;
     });
     return <View>{actions}</View>;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "rgba(247,247,247,1.0)"
+  },
+  item: {
+    padding: 5,
+    fontSize: 18,
+    height: 44,
+    borderColor: "gray",
+    borderWidth: 1
+  }
+});
